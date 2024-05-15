774348885f47092fd573f359813993fef2f19e5d
shrutipatel9 <shrutikamani024@gmail.com>
2024-05-15 17:43:38 +0530

    1s Commit

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,25 @@
+import { ActionReducerMap, createReducer, on } from "@ngrx/store";
+import { User } from "../model/user.model";
+import { AuthActions } from "../actions-types";
+
+export const authFeatureKey = "auth";
+
+export interface AuthState {
+  user: User;
+}
+
+export const initialAuthState: AuthState = {
+  user: undefined,
+};
+export const reducers: ActionReducerMap<AuthState> = {
+  user: undefined,
+};
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user,
+    };
+  })
+);
